5967eca301eff83e67494d74c2abcaeeda18402e
wrst mac <gbpleq1@gmail.com>
2022-08-24 16:04:46 +0700

    исправил именование константы кол-ва фильмов

--- src/model/films-model.js
+++ src/model/films-model.js
@@ -1,8 +1,8 @@
 import { generateFilm } from '../mock/film.js';
-const filmsCount = 5;
+const FILMS_COUNT = 5;
 
 export default class FilmsModel {
-  #films = Array.from({ length: filmsCount }, generateFilm);
+  #films = Array.from({ length: FILMS_COUNT }, generateFilm);
 
   get films() {
     return this.#films;
